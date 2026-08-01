b490d63a0dcda59a66a9da39955a72ac81e171b9
app-yevgeni <23049337+yevgeni-max2006@users.noreply.github.com>
2026-08-01 21:53:30 +0300

    update
    Updated the local execution resources to wait for ClickHouse readiness and create a database if it doesn't exist.

---
+++ modules/local-exec/main.tf
@@ -0,0 +1,55 @@
+
+resource "null_resource" "wait_for_clickhouse" {
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+
+    command = <<EOT
+set -e
+
+echo "Waiting for ClickHouse pods..."
+
+kubectl wait \
+  --namespace clickhouse \
+  --for=condition=Ready \
+  pod \
+  -l clickhouse.altinity.com/chi=clickhouse \
+  --timeout=600s
+
+echo "ClickHouse is ready."
+EOT
+  }
+
+  triggers = {
+    release = helm_release.clickhouse.version
+  }
+}
+
+
+resource "null_resource" "clickhouse_database" {
+  depends_on = [
+    null_resource.wait_for_clickhouse
+  ]
+
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+
+    command = <<EOT
+set -e
+
+POD=$(kubectl get pods -n clickhouse \
+  -l clickhouse.altinity.com/chi=clickhouse \
+  -o jsonpath='{.items[0].metadata.name}')
+
+echo "Using ClickHouse pod: $POD"
+kubectl exec -n clickhouse $POD -- \
+  clickhouse-client \
+  --query "CREATE DATABASE IF NOT EXISTS yevgeni"
+
+echo "Database yevgeni ensured."
+EOT
+  }
+
+  triggers = {
+    database = "yevgeni"
+  }
+}
